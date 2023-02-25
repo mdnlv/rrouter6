7d2f6500d9a4e9bb36a65ecdc5bb279f2efdc897
Mikhail Danilov <mikdnlv@gmail.com>
2023-02-25 09:33:19 +0300

    first part

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+//import App from './App'
 import './index.css'
+
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root"
+import ErrorPage from "./error-page"
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact"
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
